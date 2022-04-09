ea345cb26832693a2ee38924a1285f023e54e0d0
xiuji zou <zouxiuji@gmail.com>
2022-04-09 18:50:46 -0400

    drag

--- src/components/GameBoard/index.tsx
+++ src/components/GameBoard/index.tsx
@@ -18,7 +18,7 @@ const GameBoard: React.FunctionComponent = () => {
         }
     }, [])
     const handleMouseUp = (event: MouseEvent) => {
-        //TODO: 应该全局（window）检测鼠标抬起来。
+        //TODO: 应该全局（window）检测鼠标抬起来
         if(state.movingCard){
             dispatch({type: 'moveEnd', data: {mousePageX: event.pageX, mousePageY: event.pageY}})
         }
